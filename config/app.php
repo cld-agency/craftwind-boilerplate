b1d4fed688b734b280c5f0dedcfc68040709edd1
James Smith <james@cld.agency>
2021-08-20 09:05:28 +0100

    adding local smtp override for using something like Mailhog.

--- config/app.php
+++ config/app.php
@@ -18,11 +18,39 @@
  */
 
 use craft\helpers\App;
+use craft\mail\transportadapters\Smtp;
 
 return [
-    'id' => App::env('APP_ID') ?: 'CraftCMS',
-    'modules' => [
-        'my-module' => \modules\Module::class,
-    ],
-    //'bootstrap' => ['my-module'],
+	'*' => [
+		'id' => App::env('APP_ID') ?: 'CraftCMS',
+		'modules' => [
+			'my-module' => \modules\Module::class,
+		],
+		//'bootstrap' => ['my-module'],
+	],
+	'local' => [
+		// override local env to use Mailhog SMTP via MAMP Pro.
+		// these overrides are not visible in the Craft CP, and
+		// also do not get used when using the Test button on
+		// the email settings screen.
+		'components' => [
+			'mailer' => function() {
+				$settings = craft\helpers\App::mailSettings();
+				$settings->transportType = Smtp::class;
+				$settings->transportSettings = [
+					'host' => App::env('SMTP_HOST'),
+					'port' => App::env('SMTP_PORT'),
+					'useAuthentication' => App::env('SMTP_AUTHENTICATION') ?? '',
+					'username' => App::env('SMTP_USERNAME') ?? '',
+					'password' => App::env('SMTP_PASSWORD') ?? '',
+					'encryptionMethod' => App::env('SMTP_ENCRYPTION') ?? '',
+					'timeout' => App::env('SMTP_TIMEOUT') ?? 10
+				];
+
+				$config = craft\helpers\App::mailerConfig($settings);
+
+				return Craft::createObject($config);
+			}
+		]
+	]
 ];
