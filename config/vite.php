fe70dd91fb793d5afa5db7217f1988f065da4041
Neil Anderson <neil@cld.agency>
2024-12-05 09:10:34 +0000

    Fine tune vite setup

--- config/vite.php
+++ config/vite.php
@@ -2,11 +2,32 @@
 
 use craft\helpers\App;
 
+/**
+ * More docs here
+ * https://nystudio107.com/blog/using-vite-js-next-generation-frontend-tooling-with-craft-cms
+ */
 return [
+	/**
+	 * This setting controls whether or not the Vite plugin will attempt to load
+	 * files and resources from the running Vite dev server. If `true`, it will
+	 * attempt to fetch these resources even if the dev server is not running.
+	 */
 	'useDevServer' => App::env('CRAFT_ENVIRONMENT') === 'local',
+
+	/**
+	 * The internal location of the manifest file.
+	 */
 	'manifestPath' => '@webroot/dist/.vite/manifest.json',
-	'devServerPublic' => 'http://localhost:3000',
-	'devServerInternal' => 'http://localhost:3000',
-	'checkDevServer' => true,
+
+	/**
+	 * The browser-facing URL for the Vite dev server.
+	 */
+	'devServerPublic' =>
+		(!empty(App::env('VITE_SSL_KEY')) ? 'https://' : 'http://') .
+		App::env('VITE_HOST') . ':' . (App::env('VITE_PORT') ?: '3000'),
+
+	/**
+	 * The URL/path to the folder in which built files will reside.
+	 */
 	'serverPublic' => '/dist/',
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import fs from 'node:fs';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
-	return {
+	const config = {
 		// In dev mode, we serve assets at the root of http://localhost:3000
 		// In production, files live in the /assets directory
 		base: command === 'serve' ? '' : '/dist/',
@@ -16,11 +17,21 @@ export default defineConfig(({ command, mode }) => {
 			},
 		},
 		server: {
-			host: env.PRIMARY_SITE_URL.replace(/^http:\/\//, ""),
+			host: env.VITE_HOST ?? env.PRIMARY_SITE_URL.replace(/^http:\/\//, ""),
 			// Use a strict port because we have to hard code this in vite.php
 			strictPort: true,
-			port: 3000,
-			origin: 'http://localhost:3000'
+			port: env.VITE_PORT ?? 3000
 		}
+	};
+
+	if (env.VITE_SSL_CERT && env.VITE_SSL_KEY) {
+		// SSL cert/key is defined in .env file — enable Vite over SSL
+		config.server.https = {
+			key: fs.readFileSync(env.VITE_SSL_KEY),
+			cert: fs.readFileSync(env.VITE_SSL_CERT),
+			...config.server.https ?? {}
+		};
 	}
+
+	return config;
 })
