585a57ebb14834a95893ec21ca442a4958d8715a
James Smith <james@cld.agency>
2026-06-26 10:46:23 +0100

    potential fix for Vite recompiling stuff that hasn't changed

--- buildchain/src/js/modules/edit-this.js
+++ buildchain/src/js/modules/edit-this.js
@@ -1,4 +1,8 @@
-import { getCookie } from '../utils/cookies';
+function getCookie(name) {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
+}
 
 export default els => {
 	// Only proceed if logged-in-with-cp-access cookie exists and is 'true'
